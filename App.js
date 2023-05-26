c8598ce75ad8aa3a75c882458384394ca3068e56
sjsjmine129 <sjsjmine129@gmail.com>
2023-05-26 14:26:41 +0900

    test

---
+++ App.js
@@ -0,0 +1,31 @@
+import { StyleSheet, Text, View, Button } from 'react-native'; //recat native 에서 가져온 특수 헤더
+
+
+export default function App() { // app component
+  return (
+    <View style={styles.container}> 
+      <View>
+        <Text style={styles.testText}>Test the text</Text>
+      </View>
+      {/* inline style */}
+      <Text style={styles.testText}>Hello, React Native</Text>
+      <Button title="[TAP]" />
+    </View>
+    
+  );
+}
+
+const styles = StyleSheet.create({  //or inline style  -> CSS 불가능
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  testText:{
+    margin: 16, 
+    borderWidth: 2, 
+    borderColor: 'blue', 
+    padding: 16,
+  },
+});
